77fc441b12c85f3a4f3d8feb5307b9551b914364
Cesar Calva <cesar.calva90@outlook.com>
2021-02-08 10:26:19 -0500

    updated excercise folder

---
+++ Exercises/C#-Ex-03-Error_Checking.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_Exercise_01_Cesar_Calva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nPart 1 , circumference and area of a circle.");
+                    Console.Write("Enter an integer for the radius:");
+                    double intradius = getnumber();
+                    double circumference = intradius * 2 * Math.PI;
+                    Console.WriteLine($"The circumference is { circumference }");
+                    double area = Math.Pow(intradius, 2) * Math.PI;
+                    Console.WriteLine($"The area is {area}");
+                    exit = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not an Integer");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Must be Positive Integer");
+                }
+                finally
+                {
+                    Console.WriteLine("Okay");
+                }
+            } while (!exit);
+
+            exit = false;
+            do 
+            {
+                try
+                {
+                    Console.WriteLine("\nPart 2 , volume of a hemisphere.");
+                    Console.Write("Enter an integer for the radius:");
+                    double intradius = getnumber();
+                    double volume = (4.0 / 3.0 * Math.PI * Math.Pow(intradius, 3)) / 2;
+                    Console.WriteLine($"The volume is {volume}");
+                    exit = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not an Integer");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Must be Positive Integer");
+                }
+                finally
+                {
+                    Console.WriteLine("Okay");
+                }
+            } while (!exit);
+
+            exit = false;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nPart 3 , area of a triangle (Heron's formula).");
+                    Console.Write("Enter 3 integers for the 3 sides of the trinagle: \n");
+                    double intA = getnumber();
+                    double intB = getnumber();
+                    double intC = getnumber();
+                    double p = (intA + intB + intC) / 2;
+                    double area = Math.Sqrt(p * (p - intA) * (p - intB) * (p - intC));
+                    Console.WriteLine($"The area is {area}");
+                    exit = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not an Integer");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Must be Positive Integer");
+                }
+                finally
+                {
+                    Console.WriteLine("Okay");
+                }
+            } while (!exit);
+
+            exit = false;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nPart 4 , solving a quadratic equation.");
+                    Console.Write("for the quadratic equation Ax^2 + Bx + C = 0, give the parameters for A, B and C in that order: \n");
+                    double doubleA = getnumber("Allow Negative");
+                    double doubleB = getnumber("Allow Negative");
+                    double doubleC = getnumber("Allow Negative");
+                    double positive_num = Math.Sqrt(Math.Pow(doubleB, 2) - (4 * doubleA * doubleC));
+                    double negative_num = (Math.Sqrt(Math.Pow(doubleB, 2) - (4 * doubleA * doubleC))) * -1;
+                    double denominator = 2 * doubleA;
+                    double x1 = ((-1 * doubleB) + positive_num) / denominator;
+                    double x2 = ((-1 * doubleB) + negative_num) / denominator;
+                    Console.WriteLine($"x1 and x2 are respectively: \n {x1} \n {x2}");
+                    exit = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not an Integer");
+                }
+                finally
+                {
+                    Console.WriteLine("Okay");
+                }
+            } while (!exit);
+        }
+
+        private static double getnumber(string allownegative = "Deny Negative" )
+        {
+            if (allownegative == "Allow Negative")
+            {
+                Console.WriteLine("Enter integer: ");
+                string snumber = Console.ReadLine();
+                double inumber = double.Parse(snumber);
+                return inumber;
+            }                
+            else
+            {
+                Console.WriteLine("Enter positive integer: ");
+                string snumber = Console.ReadLine();
+                double inumber = double.Parse(snumber);
+                if (inumber < 0)
+                {
+                    throw new Exception();
+                }
+                return inumber;
+            }                
+        }
+    }
+}
---
+++ Exercises/C#-Ex-04-Classes_and_Objects.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace CSharp_Exercise04_Casses_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rabbits
+
+            Rabbit bugsbunny = new Rabbit("Bugs Bunny");
+            bugsbunny.Sound = "(rabbit noise)";
+            bugsbunny.Food = "Grass";
+            Rabbit.HowSpeaks(bugsbunny);
+            Rabbit.WhatEats(bugsbunny);
+            Rabbit rogerrabbit = new Rabbit("Roger Rabbit");
+            rogerrabbit.Sound = "(rabbit silence)";
+            rogerrabbit.Food = "Still Grass";
+            Rabbit.HowSpeaks(rogerrabbit);
+            Rabbit.WhatEats(rogerrabbit);
+            Rabbit thumper = new Rabbit("Thumper");
+            thumper.Sound = "(rabbit sound)";
+            thumper.Food = "Still More Grass";
+            Rabbit.HowSpeaks(thumper);
+            Rabbit.WhatEats(thumper);
+            Rabbit petercotontail = new Rabbit("Peter Cotontail");
+            petercotontail.Sound = "kackaaaaa";
+            petercotontail.Food = "Letuce";
+            Rabbit.HowSpeaks(petercotontail);
+            Rabbit.WhatEats(petercotontail);
+            
+            //Horses
+
+            Horse ed = new Horse();
+            ed.Name = "Mr. Ed";
+            ed.Sound = "neigh";
+            Horse.Introduce(ed);
+            Horse jeff = new Horse();
+            jeff.Name = "Jeff";
+            jeff.Sound = "neigh";
+            Horse.Introduce(jeff);
+            Horse steve = new Horse();
+            steve.Name = "Steve";
+            steve.Sound = "neigh";
+            Horse.Introduce(steve);
+            Horse james = new Horse();
+            james.Name = "James";
+            james.Sound = "neigh";
+            Horse.Introduce(james);
+
+            //Pigs
+
+            Pig stinky = new Pig();
+            stinky.Name = "Stinky";
+            stinky.Sound = "Oink";
+            Pig.Introduce(stinky);
+            Pig smelly = new Pig();
+            smelly.Name = "Smelly";
+            smelly.Sound = "Oink";
+            Pig.Introduce(smelly);
+            Pig dirty = new Pig();
+            dirty.Name = "Dirty";
+            dirty.Sound = "Oink";
+            Pig.Introduce(dirty);
+            Pig mudy = new Pig();
+            mudy.Name = "Mudy";
+            mudy.Sound = "Oink";
+            Pig.Introduce(mudy);
+
+            //Chickens
+
+            Chicken stan = new Chicken();
+            stan.Name = "Stan";
+            stan.Sound = "kikiriki";
+            Chicken.Introduce(stan);
+            Chicken kenny = new Chicken();
+            kenny.Name = "Kenny";
+            kenny.Sound = "kikiriki";
+            Chicken.Introduce(kenny);
+            Chicken cartman = new Chicken();
+            cartman.Name = "Cartman";
+            cartman.Sound = "kikiriki";
+            Chicken.Introduce(cartman);
+            Chicken kyle = new Chicken();
+            kyle.Name = "Kyle";
+            kyle.Sound = "kikiriki";
+            Chicken.Introduce(kyle);
+        }
+    }
+    class Rabbit
+    {
+        public string Name;
+        public string Sound;
+        public string Food;
+        public Rabbit()
+        {
+            Name = "No name given";
+        }
+        public Rabbit(string name)
+        {
+            Name = name;
+        }
+        public static void HowSpeaks(Rabbit animal)
+        {
+            Console.WriteLine($"{animal.Name} goes {animal.Sound}");
+        }
+        public static void WhatEats(Rabbit animal)
+        {
+            Console.WriteLine($"{animal.Name} eats {animal.Food}");
+        }
+    }
+    class Horse
+    {
+        public string Name;
+        public string Sound;
+        public static void Introduce(Horse animal)
+        {
+            Console.WriteLine($"My name is {animal.Name} and I am a horse. I say {animal.Sound}");
+        }
+    }
+    class Pig
+    {
+        public string Name;
+        public string Sound;
+        public static void Introduce(Pig animal)
+        {
+            Console.WriteLine($"My name is {animal.Name} and I am a pig. I say {animal.Sound}");
+        }
+    }
+    class Chicken
+    {
+        public string Name;
+        public string Sound;
+        public static void Introduce(Chicken animal)
+        {
+            Console.WriteLine($"My name is {animal.Name} and I am a chicken. I say {animal.Sound}");
+        }
+    }
+}
